d696fb4b81457faa8bf91050f41b6dc1e31546fe
JoaoVSerrano <jvserrano735@gmail.com>
2025-08-19 01:41:01 +0000

    Aula 18/08

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -2,6 +2,37 @@ package app;
 
 public class PrimeiraClasse {
     public static void main(String[] args) {
-        System.out.println("Olá, Mundo");
+        Aluno a1 = new Aluno();
+        a1.nome = "João";
+        a1.idade = 18;
+
+        Aluno a2 = a1;
+        a2.nome = "José";
+
+
+
+        System.out.println("[ A1 ]");
+        System.out.println();
+        System.out.println("nome = " + a1.nome);
+        System.out.println("idade = " + a1.idade);
+        System.out.println();
+
+        System.out.println("[ A2 ]");
+        System.out.println();
+        System.out.println("nome = " + a2.nome);
+        System.out.println("idade = " + a2.idade);
+
+        System.out.println(" [ CHAMADA DE MÉTODO ] ");
+        System.out.println("a1 = " + a1.getDados());
+        System.out.println("a2 = " + a2.getDados());
+
+        Aluno a3 = new Aluno("Maria", 35);
+        System.out.println(" [ CHAMADA DE MÉTODO ] ");
+        System.out.println("a3 = " + a3.getDados());
+
+        Cliente c1 = new Cliente();
+        c1.setNome("Teste");
+        System.out.println(" [ CHAMADA DE MÉTODO ] ");
+        System.out.println(c1.getNome());
     }
 }
